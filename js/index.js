20582b8ad91330f57402c16b80c192b8cd66bf48
Ludwig Granstedt <71264282+ludegra@users.noreply.github.com>
2022-03-14 18:07:52 +0100

    animated the menu button

--- js/index.js
+++ js/index.js
@@ -3,14 +3,21 @@ var menuButton = document.getElementById("menu-button");
 var menu = document.getElementById("main-menu");
 function playAnimation(element, duration, animationClass) {
     if (animationClass === void 0) { animationClass = "animated"; }
+    if (element.classList.contains(animationClass))
+        return;
     element.classList.toggle(animationClass);
     setTimeout(function () { return element.classList.toggle(animationClass); }, duration);
 }
-if (menuButton)
+if (menuButton) {
     menuButton.addEventListener("click", function () {
         if (menu) {
             menu.classList.toggle("active");
             menu.classList.toggle("reversed");
-            playAnimation(menu, 500);
+            playAnimation(menu, 250);
         }
+        menuButton.classList.toggle("active");
+        menuButton.classList.toggle("reversed");
+        playAnimation(menuButton, 1000);
     });
+}
+window.onload = function () { };
--- ts/index.ts
+++ ts/index.ts
@@ -1,15 +1,29 @@
 const menuButton = document.getElementById("menu-button");
 const menu = document.getElementById("main-menu");
 
-function playAnimation(element: HTMLElement, duration: number, animationClass: string = "animated") {
+function playAnimation(
+    element: HTMLElement,
+    duration: number,
+    animationClass: string = "animated"
+) {
+    if (element.classList.contains(animationClass)) return;
+
     element.classList.toggle(animationClass);
-    setTimeout(() => element.classList.toggle(animationClass), duration)
+    setTimeout(() => element.classList.toggle(animationClass), duration);
 }
 
-if (menuButton) menuButton.addEventListener("click", () => {
-    if (menu) {
-        menu.classList.toggle("active");
-        menu.classList.toggle("reversed");
-        playAnimation(menu, 500)
-    } 
-});
+if (menuButton) {
+    menuButton.addEventListener("click", () => {
+        if (menu) {
+            menu.classList.toggle("active");
+            menu.classList.toggle("reversed");
+            playAnimation(menu, 250);
+        }
+
+        menuButton.classList.toggle("active");
+        menuButton.classList.toggle("reversed");
+        playAnimation(menuButton, 1000);
+    });
+}
+
+window.onload = () => {};
